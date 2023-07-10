487d9f01649423e05be922567b1282b2cb82a69d
Archana1609 <113595255+Archana1609@users.noreply.github.com>
2023-07-10 21:37:41 +0530

    Add files via upload

---
+++ Day 3/src/Login.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Link } from "react-router-dom";
+function Login(){
+    return(
+       
+      <div>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+        <title>Document</title>
+        <link href="main.css" rel="stylesheet" type="text/css" />
+        <h1 className="h1-title">    </h1>
+        <div className="form">
+          <div className="a-group">
+            <a href="#sign-in" className="a-sign-in">Login</a>
+            <a href="#sign-up" className="a-sign-up">Register</a>
+          </div>
+          <div className="form-content">
+            <div id="sign-in">
+              <h1>  </h1>
+              <input type="email" placeholder="Email" />
+              <input type="password" placeholder="Password" />
+              <a href="#" className="frg-password">Forgot Password?</a>
+              <button className="btn-sign-in" type="submit" value="submit">Login</button>
+            </div>
+            <div id="sign-up">
+              <h1>  </h1>
+              <input type="text" placeholder="First Name" />
+              <input type="text" placeholder="Last Name" />
+              <input type="email" placeholder="Email" />
+              <input type="mobile number"placeholder="Mobile Number"/>
+              <input type="country"placeholder="Country Name"/>
+              <input type="password" placeholder="Password" />
+              <input type="confirm"placeholder="confirm password"/>
+              <button className="btn-sign-up" type="submit" value="submit">Register</button>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+    );
+}
+export default Login;
---
+++ Day 3/src/reg.js
@@ -0,0 +1,32 @@
+import React from "react";
+function reg(){
+    return(
+        <div>
+        function showSignup()
+        document.getElementById("signUp").style.display = "flex";
+        document.getElementById("login").style.display = "none";
+        document.getElementById("btns2").style.backgroundColor = "white";
+        document.getElementById("btns1").style.backgroundColor = "#cdb4db";
+        
+        function showLogin()
+        document.getElementById("signUp").style.display = "none";
+        document.getElementById("login").style.display = "flex";
+        document.getElementById("btns1").style.backgroundColor = "white";
+        document.getElementById("btns2").style.backgroundColor = "#cdb4db";
+        
+      </div>
+    );
+}
+function showSignup(){
+    document.getElementById("signUp").style.display = "flex";
+    document.getElementById("login").style.display = "none";
+    document.getElementById("btns2").style.backgroundColor = "white";
+    document.getElementById("btns1").style.backgroundColor = "#cdb4db";
+}
+function showLogin(){
+    document.getElementById("signUp").style.display = "none";
+    document.getElementById("login").style.display = "flex";
+    document.getElementById("btns1").style.backgroundColor = "white";
+    document.getElementById("btns2").style.backgroundColor = "#cdb4db";
+}
+export default reg;
